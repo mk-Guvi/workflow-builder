9537ff4438573751d1c3e7be7c18283324ca3ada
Mohammed Musharaf <mk.guvi123@gmail.com>
2025-01-11 18:47:41 +0530

    [IMPROVEMENT] added layout

---
+++ app/(workflows)/components/infobar/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const InfoBar = () => {
+  return (
+    <div className="flex flex-row gap-6 items-center px-4 py-4 w-full dark:bg-black ">
+      <h1 className="font-semibold text-lg">Automations</h1>
+    </div>
+  );
+};
+
+export default InfoBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Sidebar from "@/components/ui/sidebar";
+import InfoBar from "./(workflows)/components/infobar";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+     <div className="flex overflow-hidden h-screen">
+      <Sidebar />
+      <div className="w-full">
+      <InfoBar/>
+        {children}</div>
+    </div>
       </body>
     </html>
   );
